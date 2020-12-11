f1dca57a9305c5787b5a149cea2ef5dadb319f15
TANNU KUMARI <tannuchoudhary10@gmail.com>
2020-12-11 05:54:18 +0530

    Create 17_grid_unique_paths.cpp

---
+++ DAY3/17_grid_unique_paths.cpp
@@ -0,0 +1,91 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+ 
+
+Example 1:
+
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+Example 3:
+
+Input: m = 7, n = 3
+Output: 28
+
+Example 4:
+
+Input: m = 3, n = 3
+Output: 6
+
+ 
+
+Constraints:
+
+    1 <= m, n <= 100
+    It's guaranteed that the answer will be less than or equal to 2 * 109.
+
+*/
+//---------------------------------------------------------
+/*MY SOLUTION*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int**dp = new int*[m];
+        for(int i=0; i<m; i++){
+            dp[i] = new int[n];
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                dp[i][j] = 0;
+            }
+        }
+        
+        for(int i=0; i<m; i++)
+            dp[i][n-1] = 1;
+        
+        for(int i=0; i<n; i++)
+            dp[m-1][i] = 1;
+        
+        for(int i = m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                dp[i][j] = dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+//---------------------------------------------------------------------------------
+//OPTIMIZED SOLUTION
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int  R = m-1;
+        double res = 1;
+        for(int i=1; i<=R; i++)
+            res = res*(N-R+i)/i;
+            return (int)res;
+        
+    }
+};
+
+
+
