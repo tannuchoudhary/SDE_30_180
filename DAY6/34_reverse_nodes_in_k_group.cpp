5c2d4871d04912704a82284606581f23f8e696c5
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-01-03 13:09:18 +0530

    Create 34_reverse_nodes_in_k_group.cpp

---
+++ DAY6/34_reverse_nodes_in_k_group.cpp
@@ -0,0 +1,80 @@
+/*
+Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+Follow up:
+
+    Could you solve the problem in O(1) extra memory space?
+    You may not alter the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+
+Example 2:
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+
+Example 3:
+
+Input: head = [1,2,3,4,5], k = 1
+Output: [1,2,3,4,5]
+
+Example 4:
+
+Input: head = [1], k = 1
+Output: [1]
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range sz.
+    1 <= sz <= 5000
+    0 <= Node.val <= 1000
+    1 <= k <= sz
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || k==1) return head;
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *pre = dummy, * cur = dummy, *nex = dummy;
+        int size = 0;
+        while(cur->next!=NULL){
+            size++;
+            cur = cur->next;
+        }
+        
+        while(size>=k){
+            cur = pre->next;
+            nex = cur->next;
+            for(int i=1; i<k; i++){
+                cur->next = nex->next;
+                nex->next = pre->next;
+                pre->next = nex;
+                nex = cur->next;
+            }
+            pre = cur;
+            size -= k;
+        }   
+        return dummy->next;
+    }
+};
