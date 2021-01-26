5c2b16ea3b7e55e2e78ce1cdfada090497851457
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-01-26 09:55:56 +0530

    Create 41_trapping_rainwater.cpp

---
+++ DAY7/41_trapping_rainwater.cpp
@@ -0,0 +1,48 @@
+/*Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+Example 1:
+
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+ 
+
+Constraints:
+
+    n == height.length
+    0 <= n <= 3 * 104
+    0 <= height[i] <= 105
+
+*/
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int buildings = height.size();
+        int left = 0, right = buildings-1;
+        int resultant = 0;
+        int mxleft=0, mxright=0;
+        
+        while(left<=right){
+             if(height[left]<=height[right]){
+                 if(mxleft<height[left]) mxleft = height[left];
+                 else resultant += mxleft-height[left];
+                 left++;
+             }
+            else{
+                if(mxright<height[right]) mxright = height[right];
+                else resultant += mxright-height[right];
+                right--;
+            }
+        }
+       return resultant;
+    }
+};
