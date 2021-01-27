ac48ea337882fc64b764e8cfed2d42c09bc76b7f
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-01-27 10:02:46 +0530

    Update 97_inorder_traversal.cpp

--- DAY17/97_inorder_traversal.cpp
+++ DAY17/97_inorder_traversal.cpp
@@ -35,3 +35,49 @@ int main(){
     return 0;
     
 }
+
+//ITERATIVE
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+   public:
+   int data;
+   node* left;
+   node* right;
+   
+   node(int data){
+       this->data = data;
+       right = left = NULL;
+   }
+};
+
+void inorder(node* root){
+    stack<node*> s;
+    node* current = root;
+    
+    while(current!=NULL || s.empty()==false){
+        while(current !=NULL){
+            s.push(current);
+            current = current->left;
+        }
+        current = s.top();
+        cout<<current->data<<" ";
+        s.pop();
+        current = current->right;
+        
+    }
+    
+}
+
+int main(){
+    node* root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left  = new node(4); 
+    root->left->right = new node(5); 
+    
+    inorder(root);
+    return 0;
+}
