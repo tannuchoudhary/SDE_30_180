74ac6c470056599908fb00b7d9ea2ff962e9fce5
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-01-30 07:07:38 +0530

    Update 107_BT_balanced_or_not.cpp

--- DAY18/107_BT_balanced_or_not.cpp
+++ DAY18/107_BT_balanced_or_not.cpp
@@ -100,3 +100,32 @@ int main(){
   if(is_balanced(root, &height)) cout<<"BALANCED"<<endl;
   else cout<<"NOT BALANCED"<<endl;
 }
+
+//LEETCODE
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+        return ((abs(leftH-rightH)<=1)&&isBalanced(root->left)&&isBalanced(root->right));
+    }
+    
+    int height(TreeNode* root){
+        if(root == NULL) return 0;
+        return(max(height(root->left), height(root->right)) +1);
+    }
+};
+
+
