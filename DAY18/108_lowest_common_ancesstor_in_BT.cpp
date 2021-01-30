87be4ffab7dd699b6d69ae985e5af99c6f937501
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-01-30 06:59:08 +0530

    Update 108_lowest_common_ancesstor_in_BT.cpp

--- DAY18/108_lowest_common_ancesstor_in_BT.cpp
+++ DAY18/108_lowest_common_ancesstor_in_BT.cpp
@@ -93,3 +93,28 @@ int main(){
   cout<<find_LCA(root, 4, 5);
   return 0;
 }
+
+
+//LEETCODE
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        
+        return (!left ? right : !right ? left : root);
+    }
+};
+
+
+
