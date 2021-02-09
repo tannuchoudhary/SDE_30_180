63adfb2ab92916a162f3e4323b2cf372abcca058
TANNU KUMARI <tannuchoudhary10@gmail.com>
2021-02-09 06:29:42 +0530

    Update 118_construct_BST_from_keys.cpp

--- DAY20/118_construct_BST_from_keys.cpp
+++ DAY20/118_construct_BST_from_keys.cpp
@@ -1,1 +1,54 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+struct bst{
+   int data;
+   bst *left, *right;
+   
+   bst(int data){
+       this->data = data;
+       left = right = NULL;
+   }
+};
+
+void print_inorder(bst* root){
+    if(root == NULL)
+    return;
+    
+    print_inorder(root->left);
+    cout<<root->data<<" ";
+    print_inorder(root->right);
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+    bst *p, *q;
+    bst *root = new bst(arr[0]);
+    
+    for(int i=1; i<n; i++){
+        q = new bst(arr[i]);
+        p = root;
+        
+        while(1){
+            if(q->data <= p->data){
+                if(p->left == NULL) { 
+                    p->left = q;
+                    break;
+                }
+                else p = p->left;
+            }
+            else if(q->data > p->data){
+                if(p->right == NULL){
+                    p->right = q;
+                    break;
+                }
+                else p = p->right;
+            }
+        }
+    }
+    print_inorder(root);
+    
+}
